c8f5ea2658fdfb16119b75024a377fa341be4476
John Caruso <caruso.johnny@gmail.com>
2025-10-12 21:00:53 -0400

    initialpush

---
+++ DyingBreedModding/ModManager.cs
@@ -0,0 +1,181 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using Il2CppInterop.Runtime.Injection;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using ScriptableObjects;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using static JCDyingBreedConfigurator.Utilities;
+using HarmonyLib;
+using UnityEngine.SceneManagement;
+using UniverseLib;
+
+namespace JCDyingBreedConfigurator
+{
+    [BepInPlugin("JCDyingBreedConfigurator", "DyingBreedConfigurator", "1.0.0")]
+    public class Plugin : BasePlugin
+    {
+        #region Plugin Core
+        public GameObject DyingBreedMainModObject;
+        // Plugin startup logic
+        public override void Load()
+        {
+            Log.LogInfo("Plugin JCDyingBreedConfigurator is loaded!");
+            ClassInjector.RegisterTypeInIl2Cpp<ModManager>();
+            if (DyingBreedMainModObject == null)
+            {
+                DyingBreedMainModObject = new GameObject("DyingBreedConfiguratorMaster");
+                UnityEngine.Object.DontDestroyOnLoad(DyingBreedMainModObject);
+                DyingBreedMainModObject.hideFlags = HideFlags.HideAndDontSave;
+                DyingBreedMainModObject.AddComponent<ModManager>();
+            }
+            else
+            {
+                DyingBreedMainModObject.AddComponent<ModManager>();
+            }
+            DyingBreedMainModObject.GetComponent<ModManager>().plugin = this;
+
+            var harmony = new Harmony("JCDyingBreedConfigurator");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Universe.Init();
+        }
+        #endregion
+
+        #region HarmonyPatches
+        [HarmonyPatch(typeof(PlayerController), "Awake")]
+        static class ModPlayerController
+        {
+            [HarmonyPriority(100)]
+            private static void Postfix(PlayerController __instance)
+            {
+                ModManager.Log("PlayerControllerInjected");
+                ModManager.Instance.MainSetup();
+            }
+        }
+        #endregion
+
+        public class ModManager : MonoBehaviour
+        {
+            private static ModManager instance;
+            public string[] datalines;
+            public string[] settingslines;
+            public Plugin plugin;
+            private bool initialized;
+            private bool dataInjected;
+            private bool verboseLogging = false; 
+            public static bool VerboseLogging() => Instance.verboseLogging;
+
+
+            public string dataConfigPath;
+            public string modSettingsPath;
+            public string modRootPath;
+            public const string generatedConfigFolderPath = @"DefaultConfigData\";
+
+            //config delimiters
+            public const string dlmList = ", ";
+            public const string dlmWord = "_";
+            public const string dlmKey = ":";
+            public const string dlmComment = "//";
+            public const string dlmNewLine = "\n";
+
+            public static ModManager Instance
+            {
+                get
+                {
+                    return instance;
+                }
+            }
+
+            internal void Awake()
+            {
+                instance = this;
+            }
+
+            internal void Update()
+            {
+                if (!initialized)
+                {
+                    Instance.plugin.Log.LogInfo("locking cursor");
+                    Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                    Cursor.lockState = CursorLockMode.Confined;
+
+                    modRootPath = Directory.GetCurrentDirectory() + @"\BepInEx\plugins\DyingBreedConfigurator\";
+                    dataConfigPath = modRootPath + "ModDataConfig.txt";
+                    modSettingsPath = modRootPath + "ModSettings.txt";
+                    if (!Directory.Exists(modRootPath + generatedConfigFolderPath))
+                    {
+                        Directory.CreateDirectory(modRootPath + generatedConfigFolderPath);
+                    }
+                    initialized = true;
+                }
+
+                if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyUp(KeyCode.F10))
+                {
+                    Instance.plugin.Log.LogInfo("SendDevCMD");
+                    MainSetup();
+                }
+            }
+
+            internal void MainSetup()
+            {
+                if (dataInjected) return;
+                foreach (var unitData in RuntimeHelper.FindObjectsOfTypeAll<UnitScriptableObject>())
+                {
+                    foreach (var unitDataB in unitData.unitData)
+                    {
+                        Log(CombineStrings('"'.ToString(), unitDataB.displayName, '"'.ToString(), ","));
+                        unitDataB.Health = 8000;
+                        unitDataB.speed = 20;
+                    }
+                    //if (LocManager.legalUnitNames.Contains(unitData.name)) unitDataList.Add(unitData);
+                    //if (unitData.name == "Bear" || unitData.name == "BearSlayer") unitDataList.Add(unitData);
+                }
+                dataInjected = true;
+            }
+
+            public static string GetValue(string key)
+            {
+                string[] linesToCheck;
+                linesToCheck = Instance.datalines;
+
+                foreach (string line in linesToCheck)
+                {
+                    //ignore commented lines
+                    if (!line.Contains("//"))
+                    {
+                        if (line.Split(':')[0].Contains(key))
+                        {
+                            string value = line.Split(':')[1].TrimEnd();
+                            return value;
+                        }
+                    }
+                }
+                if (VerboseLogging()) Log(CombineStrings("Failed to find key: ", key));
+                return null;
+            }
+
+            public static void Log(string logString, int level = 1)
+            {
+                if (level == 1)
+                {
+                    Instance.plugin.Log.LogInfo(logString);
+                }
+                else if (level == 2)
+                {
+                    Instance.plugin.Log.LogWarning(logString);
+                }
+                else if (level == 3)
+                {
+                    Instance.plugin.Log.LogError(logString);
+                }
+            }
+        }
+
+    }
+}
---
+++ DyingBreedModding/Utilities.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using static JCDyingBreedConfigurator.Plugin.ModManager;
+
+namespace JCDyingBreedConfigurator
+{
+    //helper functions
+    internal class Utilities
+    {
+        private static readonly StringBuilder sb = new StringBuilder();
+        public static string CombineStrings(params string[] strings)
+        {
+            sb.Clear();
+            foreach (string s in strings)
+            {
+                sb.Append(s);
+            }
+            return sb.ToString();
+        }
+
+        public static float GetFloatByKey(float originalFloat, string key)
+        {
+            if (float.TryParse(GetValue(key), out float outVal))
+            {
+                return outVal;
+            }
+            else
+            {
+                if (VerboseLogging()) Log(CombineStrings("Failed to parse Float: ", key, dlmKey, originalFloat.ToString()));
+                return originalFloat;
+            }
+        }
+
+        public static int GetIntByKey(int originalInt, string key)
+        {
+            if (int.TryParse(GetValue(key), out int outVal))
+            {
+                return outVal;
+            }
+            else
+            {
+                if (VerboseLogging()) Log(CombineStrings("Failed to parse Int: ", key, dlmKey, originalInt.ToString()));
+                return originalInt;
+            }
+        }
+
+        public static bool GetBoolByKey(bool originalBool, string key)
+        {
+            if (bool.TryParse(GetValue(key), out bool boolVal))
+            {
+                return boolVal;
+            }
+            else
+            {
+                if (VerboseLogging()) Log(CombineStrings("Failed to parse Bool: ", key, dlmKey, originalBool.ToString()));
+                return originalBool;
+            }
+        }
+
+        public static void TryMethod(Action method)
+        {
+            try
+            {
+                method();
+            }
+            catch (Exception ex)
+            {
+                Log(CombineStrings("Failure in ", method.Method.Name, ": ", ex.Message), 3);
+            }
+        }
+
+        /// <summary>
+        /// Write to text file
+        /// </summary>
+        public static void WriteConfig(string fileName, List<string> text)
+        {
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            StreamWriter writer = new StreamWriter(fileName, true);
+            for (int i = 0; i < text.Count; i++)
+            {
+                writer.Write('\n' + text[i]);
+            }
+            writer.Close();
+        }
+
+        public static void WriteConfig(string fileName, string text)
+        {
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            StreamWriter writer = new StreamWriter(fileName, true);
+            writer.Write(text);
+            writer.Close();
+        }
+
+        public static void WriteJsonConfig(string filePath, object dataToSerialize)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true, IncludeFields = true };
+            string jsonString = JsonSerializer.Serialize(dataToSerialize, options);
+            WriteConfig(filePath, jsonString);
+        }
+
+        public static object ReadJsonConfig<T>(string filePath)
+        {
+            StreamReader reader = new StreamReader(filePath, true);
+            string jsonString = reader.ReadToEnd();
+            reader.Close();
+
+            var optionsRead = new JsonSerializerOptions { WriteIndented = true, IncludeFields = true };
+            T dataToDeserializeInto = JsonSerializer.Deserialize<T>(jsonString, optionsRead);
+            return dataToDeserializeInto;
+        }
+    }
+}
